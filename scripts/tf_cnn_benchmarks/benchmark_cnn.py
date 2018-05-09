b16dace010186083c90ad762aedcc60bc94ed45b
Mike Voets <mwhg.voets@gmail.com>
2018-05-10 01:49:29 +0200

    Add replica in front of task

--- scripts/tf_cnn_benchmarks/benchmark_cnn.py
+++ scripts/tf_cnn_benchmarks/benchmark_cnn.py
@@ -1096,7 +1096,7 @@ class BenchmarkCNN(object):
           params, create_config_proto(params))
       assert isinstance(self.cluster_manager, cnn_util.BaseClusterManager)
 
-      worker_prefix = '/job:worker/task:%s' % self.task_index
+      worker_prefix = '/job:worker/replica:%s/task:%s' % self.task_index
       if use_ps_server:
         self.param_server_device = tf.train.replica_device_setter(
             worker_device=worker_prefix + '/cpu:0',
@@ -1104,11 +1104,11 @@ class BenchmarkCNN(object):
         # This device on which the queues for managing synchronization between
         # servers should be stored.
         self.sync_queue_devices = [
-            '/job:ps/task:%s/cpu:0' % i
+            '/job:ps/replica:%s/task:%s/cpu:0' % i
             for i in range(self.cluster_manager.num_ps())
         ]
       else:
-        self.sync_queue_devices = ['/job:worker/task:0/cpu:0']
+        self.sync_queue_devices = ['/job:worker/replica:0/task:0/cpu:0']
     else:
       self.task_index = 0
       self.cluster_manager = None
@@ -1200,7 +1200,7 @@ class BenchmarkCNN(object):
       if use_ps_server:
         self.global_step_device = self.param_server_device
       else:
-        self.global_step_device = '/job:worker/task:0/cpu:0'
+        self.global_step_device = '/job:worker/replica:0/task:0/cpu:0'
     else:
       self.global_step_device = self.cpu_device
 
@@ -1213,7 +1213,7 @@ class BenchmarkCNN(object):
   def reset_devices_for_task(self, task_num, is_local=False):
     """Used to imitate another task when building a distributed graph."""
     worker_prefix = ('job:localhost'
-                     if is_local else '/job:worker/task:%s' % task_num)
+                     if is_local else '/job:worker/replica:0/task:%s' % task_num)
     self.cpu_device = '%s/cpu:0' % worker_prefix
     self.raw_devices = [
         '%s/%s:%i' % (worker_prefix, self.params.device, i)
